07d3c3e426bec54d542c35d2127dd5f19852e4ef
Ander Swift <anderswift@gmail.com>
2020-12-29 19:03:17 -0500

    feat: add authorization API module with login and register methods

---
+++ src/utils/authApi.js
@@ -0,0 +1,45 @@
+class AuthApi {
+  // temporary separate authorization api module -- will combine in next stage
+
+  constructor({ baseUrl, headers }) {
+    this._baseUrl= baseUrl;
+    this._headers= headers;
+  }
+
+
+  login(data) {
+    return fetch(this._baseUrl + 'signin', {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(data)
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      } 
+      return Promise.reject(`Error: ${res.status}`);
+    }); 
+  }
+
+
+  register(data) {
+    return fetch(this._baseUrl + 'signup', {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(data)
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      } 
+      return Promise.reject(`Error: ${res.status}`);
+    }); 
+  }
+
+}
+
+
+export const auth= new AuthApi({
+  baseUrl: "https://register.nomoreparties.co/",
+  headers: {
+    "Content-Type": "application/json"
+  }
+}); 
